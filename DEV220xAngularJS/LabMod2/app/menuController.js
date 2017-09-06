6b2d14bdd0e1e58838ef304c5b56a0a5bed07b3d
roxanavp87 <roxanavp87@gmail.com>
2017-09-05 19:32:33 -0500

    display the pizza, price and special instructions

--- DEV220xAngularJS/LabMod2/app/menuController.js
+++ DEV220xAngularJS/LabMod2/app/menuController.js
@@ -1,7 +1,23 @@
 app.controller("menuController", ["$scope", function($scope) {
     $scope.model = { title: "Our Menu" };
     $scope.changeMainDish = function(item) {
-        $scope.model.mainDish = item;
+        $scope.model.mainDish = { name: item };
+        switch (item) {
+            case "Cheese Pizza":
+                $scope.model.mainDish.price = 5.99;
+                break;
+            case "Pepperoni Pizza":
+                $scope.model.mainDish.price = 7.99;
+                break;
+            case "Margherita Pizza":
+                $scope.model.mainDish.price = 8.99;
+                break;
+            case "BBQ Chicken Pizza":
+                $scope.model.mainDish.price = 11.99;
+                break;
+            case "Combo Pizza":
+                $scope.model.mainDish.price = 13.99;
+        }
     };
     $scope.$watch('model.mainDish', function(newValue, oldValue) {
         if (newValue === 'BBQ Chicken Pizza') {
