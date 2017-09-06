eaa40f98ce36ffc757e7e8f960d3ccde44824319
roxanavp87 <roxanavp87@gmail.com>
2017-09-05 21:18:08 -0500

    working with ng-show and ng-hide

---
+++ DEV220xAngularJS/Mod3_Lab_BeginningProject/app/pizzaController.js
@@ -0,0 +1,9 @@
+app.controller('pizzaController', [
+    '$scope',
+    function($scope) {
+        $scope.model = {
+            title: 'Pizza Builder',
+            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers']
+        };
+    }
+]);
